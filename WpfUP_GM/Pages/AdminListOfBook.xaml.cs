ee4e58e00d6838e9d16ef0c08d4a1c76e72229a1
qwepubgbeer <qwepubg@mail.ru>
2026-05-07 09:41:58 +0300

    страницы для админа

---
+++ WpfUP_GM/Pages/AdminListOfBook.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUP_GM.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminListOfBook.xaml
+    /// </summary>
+    public partial class AdminListOfBook : Page
+    {
+        public AdminListOfBook()
+        {
+            InitializeComponent();
+            ProductList.ItemsSource = Core.GMEntities.Book.ToList();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(ProductList.SelectedItem!=null)
+            {
+                Book change = Core.GMEntities.Book.Find(ProductList.SelectedItem as Book);
+                if (change != null) 
+                {
+                    change.IsActive = change.IsActive? false : true;
+                    Core.GMEntities.SaveChanges();
+                }
+                ProductList.ItemsSource = Core.GMEntities.Book.ToList();
+            }
+        }
+    }
+}
---
+++ WpfUP_GM/Pages/AdminListOfRewiew.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUP_GM.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminListOfRewiew.xaml
+    /// </summary>
+    public partial class AdminListOfRewiew : Page
+    {
+        public AdminListOfRewiew()
+        {
+            InitializeComponent();
+            ProductList.ItemsSource = Core.GMEntities.Rewiew.ToList();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProductList.SelectedItem != null)
+            {
+                Rewiew change = Core.GMEntities.Rewiew.Find(ProductList.SelectedItem as Rewiew);
+                if (change != null)
+                {
+                    //change.IsActive = change.IsActive ? false : true;
+                    //Core.GMEntities.SaveChanges();
+                }
+                ProductList.ItemsSource = Core.GMEntities.Rewiew.ToList();
+            }
+        }
+    }
+}
